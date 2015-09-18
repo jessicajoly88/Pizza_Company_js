8264613f51636883f0fbc99084ae5b987cc897eb
Jessica <jessicajoly88@gmail.com>
2015-09-18 13:38:06 -0700

    Add all initial jQuery functionality and everything works in browser.

--- js/scripts.js
+++ js/scripts.js
@@ -24,13 +24,40 @@ Pizza.prototype.totalCost = function() {
   return this.totalPrice = this.dimension + this.topping;
 };
 
-$(document).ready(function() { 
-  var inputtedDimension =$("select#dimension").val();
-  
-  var inputtedToppingAmount =$(this).find("input.topping-number").val();
-  var inputtedTopping =$("select#topping").val();
 
-  if(inputtedDimension === "small" && inputtedTopping === "topping") {
-  	var toppingAmount = parseInt(inputtedToppingAmount);
+$(document).ready(function() { 
+  $("form.pizza-order").submit(function(event) {
+    event.preventDefault();
   
-}
+    var inputtedDimension =$("select#dimension").val();
+  
+    var newPizza = new Pizza("Dimension", 0);
+    var inputtedToppingAmount =$(this).find("input.topping-number").val();
+    var inputtedTopping =$("select#topping").val();
+
+    if(inputtedDimension === "small" && inputtedTopping === "topping") {
+  	  var basePrice = newPizza.smallDimension();
+  	  var toppingAmount = parseInt(inputtedToppingAmount);
+  	  var topping = newPizza.addTopping();
+  	  var toppingTotal = topping * toppingAmount;
+  	  var totalPizzaCost = basePrice + toppingTotal;	
+  	  $("#pizza-price").text("Total Amount" + " $" + totalPizzaCost);
+    }
+    else if (inputtedDimension === "medium" && inputtedTopping === "topping") {
+      var basePrice = newPizza.mediumDimension();
+  	  var toppingAmount = parseInt(inputtedToppingAmount);
+  	  var topping = newPizza.addTopping();
+  	  var toppingTotal = topping * toppingAmount;
+  	  var totalPizzaCost = basePrice + toppingTotal;
+  	  $("#pizza-price").text("Total Amount" + " $" + totalPizzaCost);	 
+    }
+    else if (inputtedDimension === "large" && inputtedTopping === "topping") {
+      var basePrice = newPizza.largeDimension();
+  	  var toppingAmount = parseInt(inputtedToppingAmount);
+  	  var topping = newPizza.addTopping();
+  	  var toppingTotal = topping * toppingAmount;
+  	  var totalPizzaCost = basePrice + toppingTotal;
+      $("#pizza-price").text("Total Amount" + " $" + totalPizzaCost);	 
+    } 
+  });
+});
